22ee7f2ca7df23e131d428db538a3aba06bfbbf7
Will Hawks <will_hawk@intuit.com>
2025-04-21 08:07:42 -0500

    add facebook provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,15 @@
-import Google from 'next-auth/providers/google';
-
 import type { NextAuthConfig } from 'next-auth';
-
+import Facebook from 'next-auth/providers/facebook';
+import Google from 'next-auth/providers/google';
 
 // Notice this is only an object, not a full Auth.js instance
 
 export default {
   providers: [
     Google,
+    Facebook({
+      clientId: process.env.AUTH_FACEBOOK_ID,
+      clientSecret: process.env.AUTH_FACEBOOK_SECRET,
+    }),
   ],
 } satisfies NextAuthConfig;
